2b7f45b7e70b43e71376b96a70d6932519f4fa1d
Krutie Patel <krutie.patel@gmail.com>
2021-11-22 02:42:29 +1000

    remove comments

--- server/redis.ts
+++ server/redis.ts
@@ -4,12 +4,10 @@ import config from '#config';
 
 const client = new Redis(config.UPSTASH_REDIS_CONN)
 
-// TODO: Rename method to incrementCount
 export const incrementCount = (urlPartial: string) => {
     return client.zincrby('myPageCounts', 1, urlPartial)
 }
 
-// TODO: Rename method to getPageCount
 export const getPageCount = (urlPartial: string) => {
     return client.zscore('myPageCounts', urlPartial)
 }
